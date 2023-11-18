52cf7942998320dcfe4f31833e5888850a36dbb7
sweetSachinKumar <k6617626@gmail.com>
2023-11-19 05:04:04 +0530

    final change
    change it

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const url = "mongodb://127.0.0.1:27017/myAPI"
+// const url = "mongodb://127.0.0.1:27017/myAPI"
 
 
 
-const connectDB = async () => {
+const connectDB = async (url) => {
     console.log("connected to mongodb")
     await mongoose.connect(url)
 }
